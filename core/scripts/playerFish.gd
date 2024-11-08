5578bb37d9d2af44fa0d69cb5d04c0ff9a8fe26c
Erica Mauriz Barundia <erica_barundia@dlsu.edu.ph>
2024-11-09 00:25:05 +0800

    added jellyfish.gd
    no code yet

--- core/scripts/playerFish.gd
+++ core/scripts/playerFish.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 250.0
 const JUMP_VELOCITY = -290.0  
-const MAX_JUMPS = 2 
+const MAX_JUMPS = 2
 var jump_count = 0  
 
 func _physics_process(delta: float) -> void:
@@ -10,15 +10,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed("jump") and jump_count < MAX_JUMPS:
-		velocity.y = JUMP_VELOCITY  # Apply the jump velocity
-		jump_count += 1  # Increment the jump counter
+		velocity.y = JUMP_VELOCITY  
+		jump_count += 1  
 
 	if is_on_floor():
 		jump_count = 0
-
+		
 	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	if direction != 0:
+		velocity.x = direction * SPEED  
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
