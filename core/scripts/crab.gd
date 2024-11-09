aa7fe1386efe283abb8a6e1c64a198ba4e7bfc89
Erica Mauriz Barundia <erica_barundia@dlsu.edu.ph>
2024-11-09 23:40:21 +0800

    crab enemy

--- core/scripts/crab.gd
+++ core/scripts/crab.gd
@@ -17,34 +17,51 @@ enum states{
 
 func _ready() -> void:
 	print("enemy start")
-	rng.randomizer()
+	rng.randomize()
 	input_direction_x = 1
 	state = states.WALK
 	$MoveTimer.start()
 
 func get_random_direction() -> int:
-	pass
+	var random_number = rng.randi_range(0,1)
+	return[-1, 1][random_number]
 
 func set_direction_right() -> void:
-	pass
+	direction = "right"
+	$AnimatedSprite2D.flip_h = false
 
 func set_direction_left() -> void:
-	pass
+	direction = "left"
+	$AnimatedSprite2D.flip_h = true
 
 func update_direction(direction_x) -> void:
-	pass
+	if direction_x > 0:
+		set_direction_right()
+	elif direction_x < 0:
+		set_direction_left()
+
 
 func _physics_process(delta: float) -> void:
-	
 	match state:
 		states.WALK:
 			$AnimatedSprite2D.play("Walk")
 			update_direction(input_direction_x)
-			pass
+			velocity.x = walk_speed * input_direction_x * delta
+			apply_gravity(delta)
+			move_and_slide()
 		
 		states.DEATH:
-			pass
-	pass
+			$AnimatedSprite2D.play("Death")
+			$body.disabled = true
+			await $AnimatedSprite2D.animation_finished
+			queue_free()
 
 func apply_gravity(delta) -> void:
-	pass
+	velocity.y += gravity * delta
+
+func _on_move_timer_timeout() -> void:
+	input_direction_x = get_random_direction()
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.owner is Player:
+		state = states.DEATH
--- core/scripts/playerFish.gd
+++ core/scripts/playerFish.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
+class_name Player
+
 const SPEED = 250.0
 const JUMP_VELOCITY = -290.0  
 const MAX_JUMPS = 2
 var jump_count = 0  
 
 @onready var fish = $AnimatedSprite2D
+
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -25,5 +29,8 @@ func _physics_process(delta: float) -> void:
 		fish.flip_h = direction < 0	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if Input.is_action_just_pressed("spit"):
+		$AnimationPlayer.play("spit")
+		
 	move_and_slide()
