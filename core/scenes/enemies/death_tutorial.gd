a9e7f45d780e6c3568eb1ce6ae4dc4ad9b777e4e
Erica Mauriz Barundia <erica_barundia@dlsu.edu.ph>
2024-11-10 03:17:34 +0800

    fixed tutorial.tcsn
    to do: turtle sprite, 2d to 3d teleportation

--- core/scenes/enemies/death_tutorial.gd
+++ core/scenes/enemies/death_tutorial.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-@onready var timer: Timer = $Timer
+#@onready var timer: Timer = $"../Timer"
 
-func _on_body_entered(body: Node2D) -> void:
-	timer.start()
+#func _on_body_entered(body: Node2D) -> void:
+	#timer.start()
 
-func _on_timer_timeout() -> void:
-	get_tree().reload_current_scene()
+#func _on_timer_timeout() -> void:
+	#get_tree().reload_current_scene()
