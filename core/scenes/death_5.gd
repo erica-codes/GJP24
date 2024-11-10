7120889e06be51a801dde51fd5fde423b26d99f2
Erica Mauriz Barundia <erica_barundia@dlsu.edu.ph>
2024-11-10 20:07:55 +0800

    end demo

--- core/scenes/death_5.gd
+++ core/scenes/death_5.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
-@onready var timer: Timer = $Timer
-
-func _on_body_entered(body: Node2D) -> void:
-	timer.start()
+@onready var timer: Timer = $"../Timer"
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and body.name == "fish":  # Replace "PlayerFish" with your player's name if necessary
+		timer.start()
