3354ca728b36d4c09834d9e9d67fcb5463a2a22b
Erica Mauriz Barundia <erica_barundia@dlsu.edu.ph>
2024-11-10 14:23:21 +0800

    MADE THE FUCKING 2D TUTORIAL TRANSITION TO 3D
    im the goat

--- core/scripts/fish3D.gd
+++ core/scripts/fish3D.gd
@@ -5,20 +5,12 @@ const JUMP_VELOCITY = Vector3(0, 7.0, 0)
 const MAX_JUMPS = 2
 var jump_count = 0
 
-@onready var camera = $fishCamera
-
-var rotation_speed = 3.0
-var camera_offset = Vector3(0, 5.0, -10.0)
-
 var smooth_rotation_speed = 5.0
 var smooth_move_speed = 0.1
 
 var target_velocity = Vector3.ZERO
 var target_rotation = Vector3.ZERO
 
-func _ready():
-	print(camera)
-	camera.position = position + camera_offset
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -46,13 +38,7 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED * delta)
 
 	move_and_slide()
-	update_camera_position(delta)
 
 func rotate_character(direction: Vector3) -> void:
 	var target_y_rotation = atan2(direction.x, direction.z)
 	rotation.y = lerp_angle(rotation.y, target_y_rotation, smooth_rotation_speed * get_physics_process_delta_time())
-
-func update_camera_position(delta: float):
-	var target_position = position + camera_offset.rotated(Vector3(0, 1, 0), rotation.y)
-	camera.position = camera.position.lerp(target_position, smooth_move_speed * delta)
-	camera.look_at(position, Vector3(0, 1, 0))
